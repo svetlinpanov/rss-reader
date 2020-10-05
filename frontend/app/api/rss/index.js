6aeb50367ecc8a3598ef3543bd636671481b8150
svetlinpanov <forteressa@gmail.com>
2020-10-05 13:52:49 +0300

    docker-compose finally working with the fronend

--- frontend/app/api/rss/index.js
+++ frontend/app/api/rss/index.js
@@ -1,38 +1,37 @@
+import axios from 'axios';
+
 import { getRssItemsMapper } from './mappers';
 export { RssAPIFake } from './fake';
 
-const apiUrl = 'http://localhost:8080/api/v1';
+const apiUrl = '/api/v1';
 
 export class RssAPI {
   postRssUrls = async urls => {
     const requestOptions = {
-      method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(urls),
     };
-    const response = await fetch(`${apiUrl}/rss`, requestOptions);
-    const data = await response.json();
-    return data;
+    const response = await axios.post(
+      `${apiUrl}/rss`,
+      JSON.stringify(urls),
+      requestOptions,
+    );
+    return response.data;
   };
 
   getRssUrls = async () => {
     const requestOptions = {
-      method: 'GET',
       headers: { 'Content-Type': 'application/json' },
     };
-    const response = await fetch(`${apiUrl}/rssUrls`, requestOptions);
-    const data = await response.json();
-    return data;
+    const response = await axios.get(`${apiUrl}/rssUrls`, requestOptions);
+    return response.data;
   };
 
   getRssItems = async () => {
     const requestOptions = {
-      method: 'GET',
-      // headers: { 'Content-Type': 'application/json' },
+      headers: { 'Content-Type': 'application/json' },
     };
-    const response = await fetch(`${apiUrl}/rss`, requestOptions);
-    const data = await response.json();
-    const rssItems = getRssItemsMapper(data);
+    const response = await axios.get(`${apiUrl}/rss`, requestOptions);
+    const rssItems = getRssItemsMapper(response.data);
     const sortedItems = rssItems.sort((a, b) => b.publishDate - a.publishDate);
     return sortedItems;
   };
